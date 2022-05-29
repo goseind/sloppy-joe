d55585b933180b7d0edac28b4fb8805e4f2ac476
Domenic Gosein <domenic.gosein@stud.hs-mannheim.de>
2022-05-30 00:03:39 +0200

    add src and update readme

---
+++ src/arduino_secrets.h
@@ -0,0 +1,9 @@
+// Fill in  your WiFi networks SSID and password
+#define SECRET_SSID      ""
+#define SECRET_PASS      ""
+
+// Fill in the hostname of your Azure IoT Hub broker
+#define SECRET_BROKER    "sloppy-joe-test-again.azure-devices.net"
+
+// Fill in the device id
+#define SECRET_DEVICE_ID "testd"
